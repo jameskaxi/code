c3bae57b956ad7426eab8fe2f3ee91cefcb8a920
wenyuankai <997230516@qq.com>
2017-12-12 22:42:17 -0800

    17-12-13

--- evm_6678/fuseRelaunch.cmd
+++
@@ -1,1 +0,0 @@
--intstyle "ise" -incremental -lib "unisims_ver" -lib "unimacro_ver" -lib "xilinxcorelib_ver" -o "D:/FPGA/1614/evm_6678/spi_tb_isim_beh.exe" -prj "D:/FPGA/1614/evm_6678/spi_tb_beh.prj" "work.spi_tb" "work.glbl" 
